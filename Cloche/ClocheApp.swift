51b9ef21dbf519f6a5adb7afe3d5291d46b9c65b
SunJae Lee <snjlee58@gmail.com>
2022-06-28 23:14:02 +0900

    Initial Commit

---
+++ Cloche/ClocheApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClocheApp.swift
+//  Cloche
+//
+//  Created by 이선재 on 2022/06/28.
+//
+
+import SwiftUI
+
+@main
+struct ClocheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
